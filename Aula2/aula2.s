0f102fe30d44dde1595e6f2f914845417761f9b9
ottozinho <wesley.otto@hotmail.com>
2016-03-14 16:00:51 -0300

    Aula2 arquivo correto

--- Aula2/aula2.s
+++ Aula2/aula2.s
@@ -15,9 +15,10 @@ _start:
         mov     $4, %eax                      # system call 4 is write
         mov     $1, %ebx                      # file handle 1 is sdout
         mov     $message, %ecx                # address of string to output
-        mov     %13, %edx                     #
-        int     %0x80                         # invoke operating system code
+        mov     $13, %edx                     #
+        int     $0x80                         # invoke operating system code
 
         # exit (0)
         mov     $1, %eax                      # system call 1 is exit
-        xor     %ebx, %ebex                   # invoke oiperating system code   
+        xor     %ebx, %ebx                    #we want return code 0
+        int     $0x80                         # invoke operating system code
