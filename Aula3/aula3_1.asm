1804b09d569c4a6eac6ea7a63c8e91600658dc0c
ottozinho <wesley.otto@hotmail.com>
2016-03-21 15:18:02 -0300

    Correto

--- Aula3/aula3_1.asm
+++ Aula3/aula3_1.asm
@@ -1,14 +1,14 @@
       SECTION .data        ;data section
 
 char:   db 'A' ; the string to print
-;char   db 0x41; the string to print
-;char   db 41h; the string to print
-;char   db 65; the string to print
+;char:   db 0x41; the string to print
+;char:   db 41h; the string to print
+;char:   db 65; the string to print
 
 
       SECTION .text         ; code section
 
-    global_start
+    global _start
 
 ;Program entry point
 
@@ -16,9 +16,9 @@ char:   db 'A' ; the string to print
 
     mov edx,1       ;
     mov ebx,1       ; arg1, where to write, screen
-    mov ecx,char    ; arg2 pointer to string 
+    mov ecx,char    ; arg2 pointer to string
     mov eax,4       ; write syout command to int 80 hex
-    int 0x40        ; interrupt 80 hex, call kernel
+    int 0x80        ; interrupt 80 hex, call kernel
 
     mov ebx,0       ; exit code, 0 = normal
     mov eax,1       ; exit command to kernel
