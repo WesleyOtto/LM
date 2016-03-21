37a8042ea689eaeea19a4f776b3943e65a175dad
ottozinho <wesley.otto@hotmail.com>
2016-03-21 15:26:49 -0300

    Correto

--- Aula3/aula3_1.asm
+++ Aula3/aula3_1.asm
@@ -14,10 +14,10 @@ char:   db 'A' ; the string to print
 
     _start:
 
-    mov edx,1       ;
-    mov ebx,1       ; arg1, where to write, screen
-    mov ecx,char    ; arg2 pointer to string
-    mov eax,4       ; write syout command to int 80 hex
+    mov edx,1       ; tamanho
+    mov ebx,1       ; arg1, where to write, screen (monitor)
+    mov ecx,char    ; arg2 pointer to string ( guarda o a)
+    mov eax,4       ; write syout command to int 80 hex (escreve a saida)
     int 0x80        ; interrupt 80 hex, call kernel
 
     mov ebx,0       ; exit code, 0 = normal
